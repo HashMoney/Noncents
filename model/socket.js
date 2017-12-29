3acd9d0404193dd3ef12a91b0dd1675f9fe7e305
SethDonohue <donohue.seth@gmail.com>
2017-12-29 14:18:49 -0800

    correct post route to sendstatus of 204 only

--- model/socket.js
+++ model/socket.js
@@ -2,8 +2,8 @@
 
 const mongoose = require('mongoose');
 
-const socketSchema = mongoose.Schema({
+const webSocketSchema = mongoose.Schema({
   address : {type : String, required : true},
 });
 
-module.exports = mongoose.model('socket', socketSchema);
+module.exports = mongoose.model('socket', webSocketSchema);
