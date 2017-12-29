25802edeca977619dc4017db2de531fce3022e6f
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-12-29 14:53:33 -0800

    testing is red for post routes

---
+++ __test__/socket-router.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+require('./lib/setup');
+
+const server = require('../lib/server');
+const superagent = require('superagent');
+
+const apiURL = `http://localhost:${process.env.PORT}`;
+
+describe('/socket routes', () => {
+  beforeAll(server.start);
+  afterAll(server.stop);
+
+  describe('post', ()=> {
+    test('post should post to the server and response should be 204 if no error', () => {
+      let addressToPost = '209.210.157.165:7777';
+
+      return superagent.post(`${apiURL}/socket`)
+        .send(addressToPost)
+        .then(response => {
+          expect(response.status).toEqual(204);
+        });
+    });
+  });
+});
