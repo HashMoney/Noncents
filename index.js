e95aadf55e2208ecb5905ff56fc25aadc6de2f72
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-12-30 16:17:35 -0800

    promises arent waiting

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 'use strict';
 
 let Chain = require('./model/chain');
+let Block = require('./model/block');
+
+
 
 let testChain = new Chain();
-
-testChain.makeBlockHash(1, 2, 3, 4);
+// testChain.currentChainArray = ['happy'];
+testChain.currentChainArray.push(new Block (0, 'genesis', new Date(), 'ledger', 'asdfgasdfasdgasdf'));
+// testChain.makeBlockHash(1, 2, 3, 4);
+testChain.currentChainArray.push(testChain.makeNextBlock('sethIsBadAtPingPong, worse than nick'));
+console.log(testChain.currentChainArray);
--- model/chain.js
+++ model/chain.js
@@ -7,29 +7,55 @@ const Block = require('./block');
 const bcrypt = require('bcrypt');
 
 const chainSchema = mongoose.Schema({
-  root: { type: Object, required: true },
+  currentChainArray: [],
 });
 
-chainSchema.methods.makeNextBlock = (ledger) => {
-  let latestBlock = this.root[this.root.length - 1];
-
+chainSchema.methods.makeNextBlock = function(ledger){
+  let latestBlock = this.currentChainArray[this.currentChainArray.length - 1];
+  console.log(latestBlock);
   return this._makeNextBlock(latestBlock, ledger);
 };
 
-chainSchema.methods._makeNextBlock = (latestBlock, ledger) => {
+chainSchema.methods._makeNextBlock = function(latestBlock, ledger){
   let nextIndex = latestBlock.index + 1;
   let timestamp = new Date();
-  let currentHash = this.makeBlockHash(nextIndex, timestamp, latestBlock.currentHash, ledger); //TODO: create this method
-  
-  return new Block(index, previousHash, timestamp, ledger, currentHash);
+  let currentHash = this.makeBlockHash(nextIndex, timestamp, latestBlock.currentHash, ledger);
+
+
+  if(typeof currentHash === 'string'){
+    console.log('current hash is ----------', currentHash);
+    return new Block(nextIndex, latestBlock.currentHash, timestamp, ledger, currentHash);
+
+  }
 };
 
-chainSchema.methods.makeBlockHash = (nextIndex, timestamp, currentHash, ledger) => {
+chainSchema.methods.makeBlockHash = function(nextIndex, timestamp, currentHash, ledger){
   return bcrypt.hash((nextIndex + timestamp + currentHash + ledger).toString(), HASH_SALT_ROUNDS)
     .then(newHash => {
-      console.log(newHash);
+      console.log('-----------newhash', newHash);
       return newHash;
     });
+};
+
+chainSchema.methods.calculateHashForBlock = function(block){
+  return this._makeBlockHash(block.nextIndex, block.timestamp, block.currentHash, block.ledger);
+};
+
+chainSchema.methods.checkBlockValidity = function(block){ //TODO: refactor console logs as error throws
+  if(!this.currentChainArray[block.index - 1]){
+    console.log('invalid index');
+    return null;
+  }
+  if(this.currentChainArray[block.index - 1].hash !== block.previousHash){
+    console.log('invalid previous hash');
+    return null;
+  }
+  if(this.calculateHashForBlock(block) !== block.hash){
+    console.log('invalid hash');
+    return null;
+  }
+  console.log('Block is valid');
+  return true;
 };
 
 module.exports = mongoose.model('chain', chainSchema);
