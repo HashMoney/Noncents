244ff72ed4bf58dc54198c03bdd90fecd1b7a8ef
Nicholas Carignan <nick.carignan@sbcglobal.net>
2018-01-03 11:27:41 -0800

    server starts up beautifully and puls down stableChain

--- __test__/block-router.test.js
+++ __test__/block-router.test.js
@@ -9,38 +9,63 @@ const Chain = require('../model/chain');
 const faker = require('faker');
 
 const apiURL = `http://localhost:${process.env.PORT}`;
+let testChain = null;
+
+let setup = function() {
+  return new Promise((resolve, reject) => {
+    return resolve();
+  })
+    .then(() => {
+      return Chain.findOne({})
+        .then(chain => {
+          testChain = chain;
+          console.log(testChain);
+          if(!testChain){
+
+            testChain = new Chain();
+            testChain.currentChainArray.push(new Block (0, 'genesis', 'genesisDate', 'genesisLedger', 'genesisHash'));
+            console.log('Genesis Block Created');
+            console.log(testChain);
+          }
+          return testChain;
+        });
+
+    });
+};
 
 describe('/block routes', () => {
   beforeAll(server.start);
+  beforeAll(setup);
   afterAll(server.stop);
 
-  let testChain = new Chain();
-  testChain.currentChainArray.push(new Block (0, 'genesis', 'genesisDate', 'genesisLedger', 'genesisHash'));
+  // testChain.currentChainArray.push(new Block (0, 'genesis', 'genesisDate', 'genesisLedger', 'genesisHash'));
 
-  // let testChain = Chain.collection.find({});
+  // let testChain = Chain.collection.findOne({});
 
 
   describe('post', ()=> {
     test('runBlockFactory should continuously build blocks and the testChain length should be the genesis block + the number of elements in the mock ledger Array', () => {
+      let length = testChain.currentChainArray.length;
       let mockFactoryLedgerArray = ['flone', 'fltwo', 'flthree', 'flfour', 'flfive'];
       return testChain.runBlockFactory(mockFactoryLedgerArray)
         .then(() => {
-          expect(testChain.currentChainArray.length).toEqual(6);
+          expect(testChain.currentChainArray.length).toEqual(length + 5);
         });
     });
 
     test('post should send ONE block to another server and respond with 204', () => {
       let mockBlock = testChain.makeNextBlock('ledger1');
-      
+
       return superagent.post(`${apiURL}/block`)
         .send(mockBlock)
         .then(response => {
           expect(response.status).toEqual(200);
         });
     });
-
-    //TODO: ADD EDGE CASE TESTS
-
-    //TODO: ADD ERROR CHECKING TESTS
   });
+
+
+  //TODO: ADD EDGE CASE TESTS
+
+  //TODO: ADD ERROR CHECKING TESTS
 });
--- route/block-router.js
+++ route/block-router.js
@@ -6,28 +6,58 @@ const httpErrors = require('http-errors');
 const Block = require('../model/block');
 const Chain = require('../model/chain');
 
-let stableChain = Chain.collection.find({});
-console.log(stableChain);
+let stableChain;
+//   .then(() => {
+//
+//     console.log(stableChain);
+//     if(!stableChain){
+//
+//       stableChain = new Chain();
+//       stableChain.currentChainArray.push(new Block (0, 'genesis', 'genesisDate', 'genesisLedger', 'genesisHash'));
+//       console.log('Genesis Block Created');
+//     }
+//
+//   });
 
-if(!stableChain){
-
-  stableChain = new Chain();
-  stableChain.currentChainArray.push(new Block (0, 'genesis', 'genesisDate', 'genesisLedger', 'genesisHash'));
-  console.log('Genesis Block Created');
-}
 
 const blockRouter = module.exports = new Router();
 
 
 blockRouter.post('/block', jsonParser, (request, response, next) => {
   //TODO: error handling
-  let blockToValidate = request.body;
-  return stableChain._addNextBlock(blockToValidate)
-    .save()
+  return new Promise((resolve, reject) => {
+    return resolve();
+  })
     .then(() => {
-      console.log('New Block Successfully Added To Chain');
-      response.send(stableChain.currentChainArray);
-      response.sendStatus(204);
-    })
-    .catch(next);
+      return Chain.findOne({})
+        .then(chain => {
+          stableChain = chain;
+          console.log(stableChain);
+          if(!stableChain){
+
+            stableChain = new Chain();
+            stableChain.currentChainArray.push(new Block (0, 'genesis', 'genesisDate', 'genesisLedger', 'genesisHash'));
+            console.log('Genesis Block Created');
+            console.log(stableChain);
+          }
+          return stableChain;
+        })
+        .then(() => {
+          let blockToValidate = request.body;
+          console.log(blockToValidate);
+          stableChain._addNextBlock(blockToValidate);
+          return stableChain.save();
+        })
+        .then(chain => console.log(chain))
+        .then(() => {
+          console.log('New Block Successfully Added To Chain');
+          response.send(stableChain.currentChainArray);
+          response.sendStatus(204);
+        })
+        .catch(next);
+
+    });
+
+
 });
+// });
