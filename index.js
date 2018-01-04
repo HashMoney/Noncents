006c6a40e45b6407a170392e827bbe7db92e0b0d
Nicholas Carignan <nick.carignan@sbcglobal.net>
2018-01-03 16:07:43 -0800

    index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 'use strict';
 
-const superagent = require('superagent');
-const Chain = require('./model/chain');
 const server = require('./lib/server');
-const WebSocket = require('./model/socket');
 
-process.env.PORT = 7000;
-process.env.MONGODB_URI = 'mongodb://heroku_61rbrfdl:auii88ojlv9kju52l3mt6qmuin@ds135777.mlab.com:35777/heroku_61rbrfdl';
-// process.env.MONGODB_URI = 'mongodb://localhost/testing';
+// process.env.PORT = 7000;
+// process.env.MONGODB_URI = 'mongodb://heroku_61rbrfdl:auii88ojlv9kju52l3mt6qmuin@ds135777.mlab.com:35777/heroku_61rbrfdl';
+// // process.env.MONGODB_URI = 'mongodb://localhost/testing';
 
 server.start();
