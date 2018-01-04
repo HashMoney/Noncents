a3e9097f3f1bf333fc1f2f04d150da8a42863962
SethDonohue <donohue.seth@gmail.com>
2018-01-04 09:44:37 -0800

    removed index port an dmognodb_uri

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 const server = require('./lib/server');
 
-process.env.PORT = 7000;
-process.env.MONGODB_URI = 'mongodb://heroku_61rbrfdl:auii88ojlv9kju52l3mt6qmuin@ds135777.mlab.com:35777/heroku_61rbrfdl';
+// process.env.MONGODB_URI = 'mongodb://heroku_61rbrfdl:auii88ojlv9kju52l3mt6qmuin@ds135777.mlab.com:35777/heroku_61rbrfdl';
+// process.env.PORT = 7000;
 // process.env.MONGODB_URI = 'mongodb://localhost/testing';
 
 server.start();
