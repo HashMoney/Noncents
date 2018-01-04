e3bc1a64b8a7f6a9bda67236fca3753c10faef43
kerrynordstrom <kerry.a.nordstrom@gmail.com>
2018-01-03 18:06:47 -0800

    Revert "Adding block factory and testing for duplicate block posting"

--- __test__/block-router.test.js
+++ __test__/block-router.test.js
@@ -7,7 +7,6 @@ const superagent = require('superagent');
 const Block = require('../model/block');
 const Chain = require('../model/chain');
 const faker = require('faker');
-const BlockMockFactory = require('./lib/mockBlock');
 
 const apiURL = `http://localhost:${process.env.PORT}`;
 let testChain = null;
@@ -70,57 +69,17 @@ describe('/block routes', () => {
     //TODO: ADD EDGE CASE TESTS
       
     //TODO: ADD ERROR CHECKING TESTS
-    test.only('post should send ONE block to another server and if index error, should respond with 400', () => {
+    test('post should send ONE block to another server and if index error, should respond with 400', () => {
       let mockBlock = testChain.makeNextBlock('ledger2');
+      // console.log('first index', mockBlock.index);
       mockBlock.index = null;
+      // console.log('second index', mockBlock.index);
       return superagent.post(`${apiURL}/block`)
         .send(mockBlock)
         .then(Promise.reject)
         .catch(response => {
-          console.log(response.message);
           expect(response.status).toEqual(400);
-        });
-    });
-    test.only('post should try to send ONE block, but should respond with 404 if wrong route used', () => {
-      let mockBlock = testChain.makeNextBlock('ledger2');
-  
-      return superagent.post(`${apiURL}/`)
-        .send(mockBlock)
-        .then(Promise.reject)
-        .catch(response => {
-          console.log(response.message);
-          expect(response.status).toEqual(404);
-        });
-    });
-
-    test.only('post should try to send ONE block, but should respond with 404 if wrong route used', () => {
-      let blockObj;
-      let blockObj2;
-      return new Promise ((resolve,reject) => {
-        return resolve();
-      })
-        .then(() => {
-          let blockObj = testChain.makeNextBlock('duplicate ledger');
-          console.log(blockObj);
-          return blockObj;
-        })
-        .then(blockObj => {
-          console.log(blockObj);
-          return superagent.post(`${apiURL}/block`)
-            .send({blockObj});
-        })
-        .then((block) => {
-          blockObj2 = block;
-          console.log(blockObj2);
-          return superagent.post(`${apiURL}/block`)
-            .send({ blockObj2});
-        })
-        .then(Promise.reject)
-        .catch(response => {
-          console.log(response.message);
-          expect(response.status).toEqual(409);
         });
     });
   });
 });
-
