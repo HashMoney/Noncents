302e55009fecdaa198d3d7f567fa70f37ef1c248
SethDonohue <donohue.seth@gmail.com>
2018-01-05 02:01:07 -0800

    removed require(socket) sockets, caused deployment ot crash

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const superagent = require('superagent');
-const Chain = require('./model/chain');
 const server = require('./lib/server');
-const WebSocket = require('./model/socket');
 
 // process.env.MONGODB_URI = 'mongodb://heroku_61rbrfdl:auii88ojlv9kju52l3mt6qmuin@ds135777.mlab.com:35777/heroku_61rbrfdl';
 // process.env.PORT = 7000;
