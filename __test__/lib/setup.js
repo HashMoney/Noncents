cb1d909fbcd5baae7cbd867594e40a7a64d7c556
Nicholas Carignan <nick.carignan@sbcglobal.net>
2018-01-23 14:40:32 -0800

    auth and tests added to heroku server

--- __test__/lib/setup.js
+++ __test__/lib/setup.js
@@ -3,3 +3,4 @@
 process.env.PORT = 7000;
 process.env.MONGODB_URI = 'mongodb://heroku_61rbrfdl:auii88ojlv9kju52l3mt6qmuin@ds135777.mlab.com:35777/heroku_61rbrfdl';
 // process.env.MONGODB_URI = 'mongodb://localhost/testing';
+process.env.CHAIN_CLOUD_SECRET = 'noncents';
